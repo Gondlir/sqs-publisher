53037d1431aeaf63e33765a009b45de3561a68af
Kaoe Ferreira <kaoeskt@hotmail.com>
2023-03-18 20:31:24 -0300

    First commit criado projeto, criado gitignore, criado modelo

---
+++ Sqs.Publisher.WebJob/EventAWSPublisher.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Sqs.Publisher.WebJob
+{
+    public sealed class EventAWSPublisher
+    {
+        public async static Task MainAsync() 
+        {
+
+        }
+    }
+}
---
+++ Sqs.Publisher.WebJob/Models/CustomerCreatedMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace Sqs.Publisher.WebJob.Models
+{
+    public sealed class CustomerCreatedMessage
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; }
+        public string Email { get; init; }
+        public string GitHubUserName { get; init; }
+    }
+}
